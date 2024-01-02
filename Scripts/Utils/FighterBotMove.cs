53764ef09842e3b87128b41b29f87ee2ee866886
SpicyOverlord <spicyoverlord@gmail.com>
2024-01-02 17:39:33 +0100

    Add comments for move classes

--- Scripts/Utils/FighterBotMove.cs
+++ Scripts/Utils/FighterBotMove.cs
@@ -2,12 +2,31 @@ using HiveBotBattle.Scripts.Utils.Types;
 
 namespace Utils
 {
+    /// <summary>
+    /// Represents a move made by a fighter bot.
+    /// </summary>
     public readonly struct FighterBotMove
     {
+        /// <summary>
+        /// The type of the fighter bot move.
+        /// </summary>
         public readonly FighterMoveType Type;
+
+        /// <summary>
+        /// The target position of the fighter bot move.
+        /// </summary>
         public readonly Pos TargetPos;
+
+        /// <summary>
+        /// Indicates whether the fighter bot move has a target position.
+        /// </summary>
         public readonly bool HasTarget;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FighterBotMove"/> struct with a target position.
+        /// </summary>
+        /// <param name="fighterMoveType">The type of the fighter bot move.</param>
+        /// <param name="targetPos">The target position of the fighter bot move.</param>
         public FighterBotMove(FighterMoveType fighterMoveType, Pos targetPos)
         {
             Type = fighterMoveType;
@@ -15,6 +34,12 @@ namespace Utils
             HasTarget = true;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FighterBotMove"/> struct with a target position specified by coordinates.
+        /// </summary>
+        /// <param name="fighterMoveType">The type of the fighter bot move.</param>
+        /// <param name="x">The x-coordinate of the target position.</param>
+        /// <param name="y">The y-coordinate of the target position.</param>
         public FighterBotMove(FighterMoveType fighterMoveType, int x, int y)
         {
             Type = fighterMoveType;
@@ -22,6 +47,10 @@ namespace Utils
             HasTarget = true;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FighterBotMove"/> struct without a target position.
+        /// </summary>
+        /// <param name="fighterMoveType">The type of the fighter bot move.</param>
         public FighterBotMove(FighterMoveType fighterMoveType)
         {
             Type = fighterMoveType;
--- Scripts/Utils/MinerBotMove.cs
+++ Scripts/Utils/MinerBotMove.cs
@@ -2,12 +2,31 @@ using HiveBotBattle.Scripts.Utils.Types;
 
 namespace Utils
 {
+    /// <summary>
+    /// Represents a move for a miner bot.
+    /// </summary>
     public readonly struct MinerBotMove
     {
+        /// <summary>
+        /// Gets a value indicating whether the move has a target position.
+        /// </summary>
         public readonly bool HasTarget;
+
+        /// <summary>
+        /// Gets the target position of the move.
+        /// </summary>
         public readonly Pos TargetPos;
+
+        /// <summary>
+        /// Gets the type of the move.
+        /// </summary>
         public readonly MinerMoveType Type;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinerBotMove"/> struct with a target position.
+        /// </summary>
+        /// <param name="minerMoveType">The type of the move.</param>
+        /// <param name="targetPos">The target position.</param>
         public MinerBotMove(MinerMoveType minerMoveType, Pos targetPos)
         {
             Type = minerMoveType;
@@ -15,6 +34,12 @@ namespace Utils
             HasTarget = true;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinerBotMove"/> struct with a target position specified by coordinates.
+        /// </summary>
+        /// <param name="minerMoveType">The type of the move.</param>
+        /// <param name="x">The x-coordinate of the target position.</param>
+        /// <param name="y">The y-coordinate of the target position.</param>
         public MinerBotMove(MinerMoveType minerMoveType, int x, int y)
         {
             Type = minerMoveType;
@@ -22,6 +47,10 @@ namespace Utils
             HasTarget = true;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinerBotMove"/> struct without a target position.
+        /// </summary>
+        /// <param name="minerMoveType">The type of the move.</param>
         public MinerBotMove(MinerMoveType minerMoveType)
         {
             Type = minerMoveType;

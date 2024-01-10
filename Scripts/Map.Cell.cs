9da166a2ee93a4063746156487dea2db68a34928
SpicyOverlord <spicyoverlord@gmail.com>
2024-01-10 15:25:55 +0100

    add gameagent to cell (only if the cell contains a gameagent)

--- Scripts/Map.Cell.cs
+++ Scripts/Map.Cell.cs
@@ -7,22 +7,24 @@ public partial class Map
     public class Cell
     {
         public readonly CellType CellType;
+        public readonly GameAgent gameAgent;
         private readonly Node2D CellNode;
         private Pos _pos;
 
         private bool _isDestroyed;
 
-        public Cell(Pos pos, Node2D node, CellType cellType)
+        public Cell(Pos pos, Node2D node, CellType cellType, GameAgent gameAgent = null)
         {
             CellNode = node;
             CellNode.Position = pos.GetAsVector2();
             _pos = pos.Clone();
 
+            this.gameAgent = gameAgent;
             CellType = cellType;
 
             _isDestroyed = false;
         }
-        
+
         public bool IsDestroyed => _isDestroyed;
         public Pos GetPosition() => _pos;
 
@@ -34,7 +36,7 @@ public partial class Map
 
         public void Destroy()
         {
-            CellNode.QueueFree();            
+            CellNode.QueueFree();
             _isDestroyed = true;
         }
     }
